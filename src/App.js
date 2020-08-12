00d8d6ebbd2e7bb3511c335d60293e7479d69a0b
ck2e14 <chriswkennedy@icloud.com>
2020-08-12 02:29:39 +0100

    (1) Added  countdown functionality: - useState 'counter' created - undefined upon first paint - after next-round-btn is clicked on endOfRoundModal, timer is given value of 5 (= seconds) - useEffect is watching counter: as soon as it's passed an integer i.e. new round starts, decrements by 1 in 1000ms intervals via setInterval - counter hitting 0 calls triggerRound('timer-elapsed') - Added extra conditional to 'determineWinner' to return opponent as winner if 'timer-elapsed' passed as arg - clicking an optionCard returns 'counter' to undefined, halting countdown. (2) Added conditional rendering of welcomeMsg/'times up'/countdown timer based on state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import OptionCard from './Components/OptionCard/OptionCard';
 import Modal from './Components/EndOfRoundModal/EndOfRoundModal';
@@ -8,12 +8,17 @@ function App() {
   const [ playerScore, setPlayerScore ] = useState(0);
   const [ opponentScore, setOpponentScore ] = useState(0);
   const [ showModal, setShowModal ] = useState(false);
+  const [ counter, setCounter ] = useState();
+  const [ firstPagePaint, setFirstPagePaint ] = useState(true);
+  // const [ rollNextRound, setRollNextRound ] = useState(false)
 
   const triggerRound = choice => {
+    setFirstPagePaint(false);
+    setCounter();
     const opponentSelection = makeOpponentSelection();
     const winner = determineWinner(choice, opponentSelection);
-    console.log(`User chose: ${choice}, Opponent chose: ${opponentSelection}, Winner is: ${winner}`)
-    endOfRoundSequence(winner, opponentSelection)
+    // console.log(`User chose: ${choice}, Opponent chose: ${opponentSelection}, Winner is: ${winner}`)
+    endOfRoundSequence(winner, opponentSelection);
   }
 
   const makeOpponentSelection = () => {
@@ -24,6 +29,7 @@ function App() {
   }
 
   const determineWinner = (choice, oppo) => {
+    if(choice === 'timer elapsed') return 'opponent'
     if(choice === oppo) return 'draw'
 
     if(choice === 'rock') { // user-chose-rock outcomes
@@ -46,16 +52,46 @@ function App() {
     if(whoWon === 'user') setPlayerScore(playerScore + 1) 
     if(whoWon === 'opponent') setOpponentScore(opponentScore + 1) 
     setShowModal(true)
+    // write function that passes oppoSelection to the modal so you can display what beat the user
   }
 
   const modalClickNextRound = () => {
+    // close modal, trigger countdown via setting a counter state value --> useEffect is watching counter state to know when to trigger 
     setShowModal(false)
+    setCounter(5)
   }
 
+    useEffect(() => {
+      const countdownInterval = setInterval(() => {
+        setCounter(counter - 1);
+      }, 1000);
+      if (counter <= 0) { 
+        triggerRound('timer elapsed') 
+        clearInterval(countdownInterval)
+      }
+      return () => clearInterval(countdownInterval);
+    }, [counter]);
+
+    const handleCountdownRender = () => {
+      if(firstPagePaint) return <div className="welcomeMsg">Make a choice to start the game</div>
+
+      if(counter === 0) return <div> Time's up! Make your choice before the timer elapses! </div> 
+      
+      if(!firstPagePaint) {
+        if(counter > 0) return <div>Countdown: {counter}</div>
+      }
+    }
+
+
   return (
-    <>  
-    { showModal && <Modal nextRoundClick={modalClickNextRound} /> }
+    <>
+    
+      { showModal && <Modal nextRoundClick={modalClickNextRound} /> }
+      {/* replace hardcoded modal with function call that returns the Modal */}
+      
       <div className="App">
+
+        {handleCountdownRender()}
 
         <div className="scorecard">
           Your Score: {playerScore} <br/>
